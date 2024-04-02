69bf201b45e40e4a12b5444011fa988e147f0eec
respowerr <jules.nr@orange.fr>
2024-04-02 12:56:12 +0200

    Initial commit

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  s3: {
+    accessKeyId: 'PB4R2TC3BQADN9D7S2OR', // TODO
+    secretAccessKey: 'gEK7SiB87D9328F8MD4NwbesX94TnlnRvd68B3wp', // TODO
+    endpoint: 'esgi-cloud-exam-bucket-45894', // TODO
+    bucket: 'esgi-cloud-exam-bucket-45894', // TODO
+  },
+  suffix: {
+    small: '_small.jpg',
+    full: '_full.jpg'
+  }
+};
